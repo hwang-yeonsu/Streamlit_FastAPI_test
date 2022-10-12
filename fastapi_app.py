3be9f23cfabf45b59ae0b2eca2346ca64fbf458e
terry <terry>
2022-10-12 14:13:25 +0900

    Initial commit

---
+++ fastapi_app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import uvicorn
+import time
+
+app = FastAPI()
+
+
+
+@app.get("/")
+def root():
+    d = {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut autem",
+        "completed": False,
+    }
+    return d
+
+
+@app.get("/psi")
+def psi():
+    def generator():
+        for i in range(3):
+            yield "some fake lines\n"
+            time.sleep(0.5)
+        yield 'psi done.'
+
+    return StreamingResponse(generator())
+
+
+if __name__=='__main__':
+    uvicorn.run('fastapi_app:app', reload=True)
---
+++ streamlit_app.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import joblib
+import requests
+import json
+import time
+import traceback
+import streamlit as st
+
+
+def call_api(path: str,
+             method: str = 'GET',
+             data: json = None,
+             stream: bool = False):
+    """API call function.
+    Args:
+        path (str): URL path.
+        method (str, optional): Http request method. Defaults to 'GET'.
+        data (json, optional): Json data in post method. Defaults to None.
+        stream (bool, optional): True if StreamingResponse. Defaults to False.
+    Returns:
+        _type_: _description_
+    """
+
+    if(method.upper()=='GET'):
+        res = requests.get(PATH + path, stream=stream)
+    else:
+        res = requests.post(PATH + path, json=data, stream=stream)
+    res.raise_for_status()
+
+    return res
+
+
+
+################################ main ################################
+
+## Lending page
+st.title('Federated Learning')
+with st.expander('Please input your server url path.', expanded=True):
+    # https://jsonplaceholder.typicode.com/todos/1
+    PATH = st.text_input('e.g) https://jsonplaceholder.typicode.com/todos/1')
+    if(not PATH):
+        st.stop()
+    st.write('Status:', call_api('/'))
+    st.success('Confirmed')
+
+
+'\n'
+'\n'
+'\n'
+
+
+
+
+
+
+
+def test(x):
+    print(x)
+
+a = st.button('a test', key='a', on_click=test, args=('a test',))
+b = st.button('b test', key='b', on_click=test, args=('b test',))
+c = st.text_input('asd', key='c')
+st.session_state.d = 'aaa'
+
+st.session_state
+
+
+
+
+
+
+
+
+## menu tabs
+tab_list = ['/root', '/psi', '/define-model']
+tab1, tab2, tab3 = st.tabs(tab_list)
+
+with tab1:
+    st.header("Root")
+    st.write('''
+        _some docs..._
+    ''')
+    btn = st.button('RUN', key='root')
+    if(btn):
+        st.write('Status:', call_api('/').json())
+    st.stop()
+
+
+with tab2:
+    st.header("PSI")
+    st.write('''
+        _some docs..._
+    ''')
+    if(st.button('RUN', key='psi')):
+        with call_api('/psi', stream=True) as res:
+            for chunk in res.iter_lines(chunk_size=1):
+                st.write(chunk.decode('utf-8'))
+
+
+with tab3:
+  st.header("Defind Model")
+  st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+
+
+
+
+
+
+
+
+# st.error('Wrong URL.', icon='')
+
+# btn = st.button('Simple status')
+# if(btn):
+#   res = requests.get(PATH)
+#   if(res.status_code==200):
+#     st.write('Status:', res.json())
+
+# "---"
+
+# ## form
+# with st.form("my_form"):
+#    st.write("Inside the form")
+#    slider_val = st.slider("Form slider")
+#    checkbox_val = st.checkbox("Form checkbox")
+#    # Every form must have a submit button.
+#    submitted = st.form_submit_button("Submit")
+#    if submitted:
+#        st.write("slider", slider_val, "checkbox", checkbox_val)
+# st.write("Outside the form")
+
+# ## inputs
+# submit_btn = st.button('Submit')
+# download_btn = st.download_button('Download', 'boo!')
+# selected = st.checkbox('I agree')
+# # print(selected)
+# choice = st.selectbox('Pick one', ['Cat', 'Dog'])
+# text_input = st.text_input('Save path')
+
+# ## dataframe & plot
+# data = { k: [ x+k for x in range(5) ] for k in range(10) }
+# st.dataframe(data)
+# st.line_chart(data)
+# st.area_chart(data)
+
+# st.balloons()
+
+# import time
+# 'Starting a long computation...'
+
+# # Add a placeholder
+# latest_iteration = st.empty()
+# bar = st.progress(100)
+
+# for i in range(100):
+#   # Update the progress bar with each iteration.
+#   latest_iteration.text(f'Iteration {i+1}')
+#   bar.progress(i + 1)
+#   time.sleep(0.1)
+
+# '...and now we\'re done!'
+
+# import time
+# with st.spinner('Wait for it...'):
+#     time.sleep(5)
+# st.success('Done!')
